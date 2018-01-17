af18a86b7c35b4a9fe6dcd00805aa2830942b947
Jonas Feller <jonas.feller@itnetx.ch>
2018-01-17 22:34:30 +0100

    fix for Server Core

--- Misc/xBitlockerCommon.psm1
+++ Misc/xBitlockerCommon.psm1
@@ -425,7 +425,8 @@ function CheckForPreReqs
         Write-Error "The RSAT-Feature-Tools-BitLocker feature needs to be installed before the xBitlocker module can be used"
     }
 
-    if ($blAdminToolsRemoteFeature.InstallState -ne "Installed")
+    if ($blAdminToolsRemoteFeature.InstallState -ne "Installed" -and (Get-OSEdition) -notmatch "Core")
+    #if ($blAdminToolsRemoteFeature.InstallState -ne "Installed")
     {
         $hasAllPreReqs = $false
 
@@ -517,5 +518,15 @@ function RemoveParameters
         }
     }
 }
+
+<#
+.SYNOPSIS
+Returns the OS edtion we currently running on
+#>
+function Get-OSEdition
+{
+    (Get-ItemProperty -Path "HKLM:/software/microsoft/windows nt/currentversion" -Name InstallationType).InstallationType
+}
+
 
 Export-ModuleMember -Function *
