930a94cfaf55126fca5fa7a46f07a904ec66f29f
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-08-28 10:54:46 +0200

    Update module files

--- source/Examples/ConfigureBitlockerAndAutoBitlocker/ConfigureBitlockerAndAutoBitlocker.ps1
+++ source/Examples/ConfigureBitlockerAndAutoBitlocker/ConfigureBitlockerAndAutoBitlocker.ps1
@@ -46,5 +46,3 @@ Configuration ConfigureBitlockerAndAutoBitlocker
         }
     }
 }
-
-ConfigureBitlockerAndAutoBitlocker
--- source/Examples/ConfigureBitlockerOnOSDrive/ConfigureBitlockerOnOSDrive.ps1
+++ source/Examples/ConfigureBitlockerOnOSDrive/ConfigureBitlockerOnOSDrive.ps1
@@ -35,5 +35,3 @@ Configuration ConfigureBitlockerOnOSDrive
         }
     }
 }
-
-ConfigureBitlockerOnOSDrive
